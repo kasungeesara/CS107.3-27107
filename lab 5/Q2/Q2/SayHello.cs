3e687bc746bb38094897c7b2dca3af947dca3a06
kasungeesara <136785383+kasungeesara@users.noreply.github.com>
2023-07-16 12:24:45 +0530

    Add files via upload

---
+++ lab 5/Q2/Q2/SayHello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class SayHello
+    {
+        private void sayHello()
+        {
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+}
