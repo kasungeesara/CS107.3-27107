e40058fe81612b762aafde36517063136dc5bbb6
kasungeesara <136785383+kasungeesara@users.noreply.github.com>
2023-07-16 12:12:24 +0530

    Add files via upload

---
+++ lab1/q1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string batch;
+            Console.WriteLine("enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("enter your batch");
+            batch =Console.ReadLine();
+            Console.WriteLine("My name is "+name);
+            Console.WriteLine("My batch is "+ batch);
+            Console.ReadLine();
+        }
+    }
+}
